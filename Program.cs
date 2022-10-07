34780f44a731016f808f7333c77cb967b32817a8
yamurzuko <yamurzcan@gmail.com>
2022-10-07 15:01:35 +0300

    Encapsulations

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace ClassConcept
     {
         static void Main(string[] args)
         {
+/*
             Worker worker1 = new Worker("Ugur", "Yagmur", 34627, "İnsan Kaynaklari");
             worker1.Workers();
             Console.WriteLine("*************");
@@ -17,9 +18,15 @@ namespace ClassConcept
             worker2.departman = "satin alma";
             worker2.Workers();
             Console.WriteLine("*************");
+*/
+            Student student1 = new Student("UğurCan", "Yağmur", 43523, 12);
+            student1.StudentInformation();
+            student1.SınıfAtlat();
+            student1.StudentInformation();
 
         }
     }
+/*
     class Worker
     {
         public string name;
@@ -43,4 +50,43 @@ namespace ClassConcept
             Console.WriteLine("Çalişanin Departmani: {0}", departman);
         }
     }
+*/  
+
+    class Student
+    {
+        private string name;
+        private string surname;
+        private int no;
+        private int sınıf;
+
+        public Student(){}
+        public Student(string name, string surname, int no , int sınıf)
+        {
+            Name = name;
+            Surname = surname;
+            No = no;
+            Sınıf = sınıf;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int No { get => no; set => no = value; }
+        public int Sınıf { get => sınıf; set => sınıf = value; }
+
+        public void StudentInformation()
+        {
+            Console.WriteLine("Öğrencinin Adı: {0}", this.Name);
+            Console.WriteLine("Öğrencinin Soyadı: {0}", this.Surname);
+            Console.WriteLine("Öğrencinin Numarası: {0}", this.No);
+            Console.WriteLine("Öğrencinin Sınıfı: {0}", this.Sınıf);
+            Console.WriteLine("******************");
+        }
+
+        public void SınıfAtlat()
+        {
+            this.Sınıf = this.Sınıf + 1;
+        }
+
+    }
+    
 }
